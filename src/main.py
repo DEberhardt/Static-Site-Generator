0fd264cf08517f4ba6eb0efe5f9bb28ac304e43c
david.eberhardt@outlook.com <david.eberhardt@outlook.com>
2025-05-03 15:09:55 +0100

    prepping LeafNode
    Signed-off-by: david.eberhardt@outlook.com <david.eberhardt@outlook.com>

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
-print("Hello World")
+def main():
+    print("This is the main function.")
+
+if __name__ == "__main__":
+    main()
+    print("TextNode(This is some anchor text, link, https://www.boot.dev)")
--- src/textnode.py
+++ src/textnode.py
@@ -7,3 +7,19 @@ class TextType(Enum):
     PLAIN = 1
     MARKDOWN = 2
     HTML = 3
+    BOLD = 4
+
+class TextNode():
+    """
+    Class representing a text node.
+    """
+    def __init__(self, text: str, text_type: TextType, url: str = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(obj1, obj2):
+        return isinstance(obj1, TextNode) and obj1.text == obj2.text and obj1.text_type == obj2.text_type and obj1.url == obj2.url
+
+    def __repr__(self):
+        return f"TextNode(text={self.text}, text_type={self.text_type}, url={self.url})"
